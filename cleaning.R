7c78de74b9351764e29fb6fc9987ab45b84784d3
Mao Lenin Soldevilla Ureta <maolenin@gmail.com>
2026-06-30 22:16:36 -0500

    Getting data

---
+++ cleaning.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+file <- "./data/original/en_US.blogs.txt"
+all_lines <- read_lines(file)
+
+set.seed(123)
+sampled_lines <- sample(all_lines, size = length(all_lines)*.05)
+
+text_data <- tibble(line_id = 1:length(sampled_lines), text = sampled_lines)
+
+# View your sampled data structure
+print(text_data)
+
+      
